75c7b60947369d86dcddbcea67cee49135ea816c
editgames-owner <editgames@yandex.com>
2019-04-30 08:32:52 +0800

    1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # EmbedTiledInForm
-How to Embed TCastleTiledMapControl in Form1?
+Thanks to eugeneloza,
+
+Embed TCastleTiledMapControl in Form1,it works now.
+
+
+
+https://forum.castle-engine.io/t/how-to-embed-tcastletiledmapcontrol-in-form1/71
+
+![itworks-screenshot](itworks-screenshot.png)
--- unit1.pas
+++ unit1.pas
@@ -5,15 +5,15 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, CastleControl,
-  CastleTiledMap;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  CastleControl, CastleTiledMap;
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
+    Button1: TButton;
     Castle2DControl1: TCastle2DControl;
-    CastleControl1: TCastleControl;
     procedure FormCreate(Sender: TObject);
   private
 
@@ -34,11 +34,15 @@ procedure TForm1.FormCreate(Sender: TObject);
 var
   MapControl: TCastleTiledMapControl;
 begin
-  MapControl := TCastleTiledMapControl.Create(CastleControl1);
-  MapControl.URL := 'castle-data:/maps/isometric_grass_and_water.tmx';
-  CastleControl1.Controls.InsertFront(MapControl);
+  MapControl := TCastleTiledMapControl.Create(Castle2DControl1);
+  MapControl.FullSize := true;
+  MapControl.URL := 'castle-data:/maps/isometric.tmx';
+
   Castle2DControl1.Controls.InsertFront(MapControl);
+  //Castle2DControl1.Controls.InsertBack(MapControl);
 end;
+
+
 
 end.
 
